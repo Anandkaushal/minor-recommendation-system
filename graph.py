d8a1fa2aec786d831470a14a9753ba880d1b03f4
Anandkaushal <63389069+Anandkaushal@users.noreply.github.com>
2021-11-23 18:44:27 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x=['Data Science','Machine Learning','Cyber Security','Mean Stack Developer','Software Methodology']
+y=[20,50,10,20,0]
+plt.figure(figsize=(12,5))
+plt.bar(x,tq)
+plt.title("Minor Recommendation Graph")
+plt.xlabel("Engineering Minors")
+plt.ylabel("Interest Distribution")
+plt.show()
+
+ 
